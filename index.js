53346c087b129a714d835d10d46ff901df3374fa
Gabriel Vilela <vilela.gabriel2@gmail.com>
2024-05-13 15:23:56 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+nome = "Parasita"
+xp = 9700
+
+if (xp < 1000){
+    console.log("O Herói de nome " + nome +  " está no nível Ferro")
+} else if (xp < 2001){
+    console.log("O Herói de nome " + nome +  " está no nível Bronze")
+} else if (xp < 5001){
+    console.log("O Herói de nome " + nome +  " está no nível Prata")
+} else if (xp < 7001){
+    console.log("O Herói de nome " + nome +  " está no nível Ouro")
+}else if (xp < 8001){
+    console.log("O Herói de nome " + nome +  " está no nível Platina")
+}else if (xp < 9001){
+    console.log("O Herói de nome " + nome +  " está no nível Ascendente")
+}else if (xp < 10001){
+    console.log("O Herói de nome " + nome +  " está no nível Radiante")
+}else{
+    console.log("O Herói de nome " + nome +  " está no nível Radiante")
+}
